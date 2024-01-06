eb5dc48acc50e758bac90299653c5c3c38bcf6dd
vyshvydu <79267593+vyshvydu@users.noreply.github.com>
2024-01-06 12:01:24 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -27,9 +27,18 @@ export default class HomeScreen extends Component {
   }
 
   getData = async () => {
-    /*write code to fetch data from your flask API.
-     Store the fetched data in the state named "data"*/
-
+ const { url } = this.state;
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({
+          datas: response.data.data,
+        });
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
+  
      
   };
 
